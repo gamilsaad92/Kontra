00a6d539bc0aa804f4f6b430f446bfee4d227606
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-21 21:38:31 -0700

    Update index.ts

--- api/api/index.ts
+++ api/api/index.ts
@@ -1,9 +1,8 @@
-// api/api/index.ts
+// Kontra/api/api/index.ts
 import serverless from 'serverless-http'
 import app from '../src/app'
 
-// Vercel runtime config
-export const config = { runtime: 'nodejs' }
+// Use Node 18 runtime
+export const config = { runtime: 'nodejs18.x' }
 
-// Wrap the Express app with serverless-http so Vercel can run it
 export default serverless(app)
